316a1dbea696e91691d52729a8d9757598e77e91
Adina Wagner <adina.wagner@t-online.de>
2021-08-07 15:21:54 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/adswa/pymento',
-    version='0.0.11',
+    version='0.0.12',
     zip_safe=False,
 )
