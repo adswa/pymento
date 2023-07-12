4f7c563b2bf392aad40d682b5e43a4836483f574
Adina Wagner <adina.wagner@t-online.de>
2023-07-12 15:03:25 +0200

    adjust sensor subsetting to modern mne

--- pymento_meg/utils.py
+++ pymento_meg/utils.py
@@ -23,15 +23,15 @@ def _get_channel_subsets(raw):
     Return defined sensor locations as a list of relevant sensors
     """
     # TODO: fill in with sensor names for left and right
-    parietal_left = mne.read_selection(name=["Left-parietal"], info=raw.info)
-    parietal_right = mne.read_selection(name=["Right-parietal"], info=raw.info)
-    occipital_left = mne.read_selection(name=["Left-occipital"], info=raw.info)
-    occipital_right = mne.read_selection(name=["Right-occipital"], info=raw.info)
-    frontal_left = mne.read_selection(name=["Left-frontal"], info=raw.info)
-    frontal_right = mne.read_selection(name=["Right-frontal"], info=raw.info)
-    vertex_sensors = mne.read_selection(name=["Vertex"], info=raw.info)
-    temporal_left = mne.read_selection(name=["Left-temporal"], info=raw.info)
-    temporal_right = mne.read_selection(name=["Right-temporal"], info=raw.info)
+    parietal_left = mne.read_vectorview_selection(name=["Left-parietal"], info=raw.info)
+    parietal_right = mne.read_vectorview_selection(name=["Right-parietal"], info=raw.info)
+    occipital_left = mne.read_vectorview_selection(name=["Left-occipital"], info=raw.info)
+    occipital_right = mne.read_vectorview_selection(name=["Right-occipital"], info=raw.info)
+    frontal_left = mne.read_vectorview_selection(name=["Left-frontal"], info=raw.info)
+    frontal_right = mne.read_vectorview_selection(name=["Right-frontal"], info=raw.info)
+    vertex_sensors = mne.read_vectorview_selection(name=["Vertex"], info=raw.info)
+    temporal_left = mne.read_vectorview_selection(name=["Left-temporal"], info=raw.info)
+    temporal_right = mne.read_vectorview_selection(name=["Right-temporal"], info=raw.info)
     sensors = {
         "lpar": parietal_left,
         "rpar": parietal_right,
